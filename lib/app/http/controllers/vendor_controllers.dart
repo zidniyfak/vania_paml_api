96f217bee2469bcdfa8f0ef8f6010bb6aa651027
Zidniy Alkausar <suratalkausar@gmail.com>
2024-12-10 22:16:20 +0700

    Mengubah vendor_controllers.dart

--- lib/app/http/controllers/vendor_controllers.dart
+++ lib/app/http/controllers/vendor_controllers.dart
@@ -17,20 +17,20 @@ class VendorControllers extends Controller {
 
   Future<Response> store(Request request) async {
     try {
-      var vendorName = request.input('vendor_name');
-      var vendorAddress = request.input('vendor_address');
-      var vendorKota = request.input('vendor_kota');
-      var vendorState = request.input('vendor_state');
-      var vendorZip = request.input('vendor_zip');
-      var vendorCountry = request.input('vendor_country');
+      var vendName = request.input('vend_name');
+      var vendAddress = request.input('vend_address');
+      var vendKota = request.input('vend_kota');
+      var vendState = request.input('vend_state');
+      var vendZip = request.input('vend_zip');
+      var vendCountry = request.input('vend_country');
 
       await Vendor().query().insert({
-        'vendor_name': vendorName,
-        'vendor_address': vendorAddress,
-        'vendor_kota': vendorKota,
-        'vendor_state': vendorState,
-        'vendor_zip': vendorZip,
-        'vendor_country': vendorCountry,
+        'vend_name': vendName,
+        'vend_address': vendAddress,
+        'vend_kota': vendKota,
+        'vend_state': vendState,
+        'vend_zip': vendZip,
+        'vend_country': vendCountry,
         'created_at': DateTime.now(),
         'updated_at': DateTime.now(),
       });
@@ -58,20 +58,20 @@ class VendorControllers extends Controller {
 
   Future<Response> update(Request request, int id) async {
     try {
-      var vendorName = request.input('vendor_name');
-      var vendorAddress = request.input('vendor_address');
-      var vendorKota = request.input('vendor_kota');
-      var vendorState = request.input('vendor_state');
-      var vendorZip = request.input('vendor_zip');
-      var vendorCountry = request.input('vendor_country');
+      var vendName = request.input('vend_name');
+      var vendAddress = request.input('vend_address');
+      var vendKota = request.input('vend_kota');
+      var vendState = request.input('vend_state');
+      var vendZip = request.input('vend_zip');
+      var vendCountry = request.input('vend_country');
 
       await Vendor().query().where('id', '=', id).update({
-        'vendor_name': vendorName,
-        'vendor_address': vendorAddress,
-        'vendor_kota': vendorKota,
-        'vendor_state': vendorState,
-        'vendor_zip': vendorZip,
-        'vendor_country': vendorCountry,
+        'vend_name': vendName,
+        'vend_address': vendAddress,
+        'vend_kota': vendKota,
+        'vend_state': vendState,
+        'vend_zip': vendZip,
+        'vend_country': vendCountry,
         'updated_at': DateTime.now(),
       });
 
